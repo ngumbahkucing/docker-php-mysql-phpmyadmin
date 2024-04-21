84f9048a019859a5e69b20a0c3aa4b897ea63b32
Taukhid Wardoyo <taukhid.wardoyo@gmail.com>
2024-04-21 10:11:25 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,16 @@
 FROM php:7.4-apache
-RUN apt-get update && apt-get upgrade -y
+RUN apt-get update 
+#RUN apt-get upgrade -y     #enable this line to upgrade packages
 RUN docker-php-ext-install pdo pdo_mysql
 RUN docker-php-ext-install calendar
 
+RUN apt-get install -y \
+    zlib1g-dev \
+    libpng-dev
+
+RUN docker-php-ext-install gd
+
 COPY $PWD/etc/php/php.ini /usr/local/etc/php
+RUN chown -R www-data:www-data /var/www/html
 
 EXPOSE 80
--- init/01-create-more-databases.sh
+++ init/01-create-more-databases.sh
@@ -1,14 +1,49 @@
 # Keep this file plain. Do not `chmod +x` to make it executable.
 
+mysql_note() {
+	mysql_log Note "$@"
+}
+docker_process_sql() {
+	passfileArgs=()
+	if [ '--dont-use-mysql-root-password' = "$1" ]; then
+		passfileArgs+=( "$1" )
+		shift
+	fi
+	# args sent in can override this db, since they will be later in the command
+	if [ -n "$MYSQL_DATABASE" ]; then
+		set -- --database="$MYSQL_DATABASE" "$@"
+	fi
+
+	mysql --defaults-extra-file=<( _mysql_passfile "${passfileArgs[@]}") --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" --comments "$@"
+}
+_mysql_passfile() {
+	# echo the password to the "file" the client uses
+	# the client command will use process substitution to create a file on the fly
+	# ie: --defaults-extra-file=<( _mysql_passfile )
+	if [ '--dont-use-mysql-root-password' != "$1" ] && [ -n "$MYSQL_ROOT_PASSWORD" ]; then
+		cat <<-EOF
+			[client]
+			password="${MYSQL_ROOT_PASSWORD}"
+		EOF
+	fi
+}
+mysql_log() {
+	local type="$1"; shift
+	# accept argument string or stdin
+	local text="$*"; if [ "$#" -eq 0 ]; then text="$(cat)"; fi
+	local dt; dt="$(date --rfc-3339=seconds)"
+	printf '%s [%s] [Entrypoint]: %s\n' "$dt" "$type" "$text"
+}
+
 create_more_databases() {
     local databases=( $MORE_DATABASES )
     local db
     for db in "${databases[@]}"; do
-        #mysql_note "Creating database ${db}"
+        mysql_note "Creating database ${db}"
         docker_process_sql --database=mysql <<<"CREATE DATABASE IF NOT EXISTS \`$db\` ;"
 
         if [ -n "$MYSQL_USER" ] && [ -n "$MYSQL_PASSWORD" ]; then
-            #mysql_note "Giving user ${MYSQL_USER} access to schema ${db}"
+            mysql_note "Giving user ${MYSQL_USER} access to schema ${db}"
             docker_process_sql --database=mysql <<<"GRANT ALL ON \`${db//_/\\_}\`.* TO '$MYSQL_USER'@'%' ;"
         
         fi
